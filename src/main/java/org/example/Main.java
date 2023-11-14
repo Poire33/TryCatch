50e5fe8415a21a86f3b12beac0ab09603f1695cd
Rupert spelled with one P <r1p2rtecd2004@aol.com>
2023-11-14 13:57:59 +0000

    Second version with try/catch working, prompts the user again each time an invalid entry is entered

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,28 +3,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Please enter a number between 0-9: ");
-        Scanner myscan = new Scanner(System.in);
-
-        // Read user input as a String
-        String input = myscan.nextLine();
 
         // Check user entry
-        try {
-            // Parse the input to an integer
-            int myNum = Integer.parseInt(input);
+        while (true) {
+            try {
+                System.out.println("Please enter a number between 0-9: ");
+                Scanner myscan = new Scanner(System.in);
 
-            // Check if the number is between 0 and 9
-            if (myNum >= 0 && myNum <= 9) {
-                System.out.println("You entered " + myNum);
+                // Read user input as a String
+                String input = myscan.nextLine();
+
+                // Parse the input to an integer
+                int myNum = Integer.parseInt(input);
+
+                // Check if the number is between 0 and 9
+                if (myNum >= 0 && myNum <= 9) {
+                    System.out.println("You entered " + myNum);
+                    break;
+                }
+                else {
+                    System.out.println("Not a valid number");
+                }
             }
-            else {
-                System.out.println("Not a valid number");
+            // Output error message if a non-numerical character is entered
+            catch (NumberFormatException character) {
+                System.out.println("You must enter a number between 0 and 9, you entered another type of character");
             }
-        }
-        // Output error message if a non-numerical character is entered
-        catch (NumberFormatException character) {
-            System.out.println("You must enter a number between 0 and 9, you entered another type of character");
         }
     }
 }
