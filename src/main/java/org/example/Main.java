5bd2540a860098921203fa2854270797b2b265ee
Rupert spelled with one P <r1p2rtecd2004@aol.com>
2023-11-14 13:46:26 +0000

    First version with try/catch working, exits when error prints out

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,15 +9,22 @@ public class Main {
         // Read user input as a String
         String input = myscan.nextLine();
 
-        // Parse the input to an integer
-        int myNum = Integer.parseInt(input);
+        // Check user entry
+        try {
+            // Parse the input to an integer
+            int myNum = Integer.parseInt(input);
 
-        // Check if the number is between 0 and 9
-        if (myNum >= 0 && myNum <= 9) {
-            System.out.println("You entered " + myNum);
+            // Check if the number is between 0 and 9
+            if (myNum >= 0 && myNum <= 9) {
+                System.out.println("You entered " + myNum);
+            }
+            else {
+                System.out.println("Not a valid number");
+            }
         }
-        else {
-            System.out.println("Not a valid number");
+        // Output error message if a non-numerical character is entered
+        catch (NumberFormatException character) {
+            System.out.println("You must enter a number between 0 and 9, you entered another type of character");
         }
     }
 }
